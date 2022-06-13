f6bc5af91e6b9cb17ea2b1011f3bb36aae766a72
Joanna <joannakhek@gmail.com>
2022-06-13 19:55:15 +0800

    added parse_mode

--- main.py
+++ main.py
@@ -132,8 +132,8 @@ if __name__ == "__main__":
         code_html='*NEW FREE PERFORMANCES IN ESPLANADE*'  
         df_update = df[df["title"].isin(update)]
         for k in range(len(df_update)):
-            msg = code_html + "\n\n Category:" + str((df_update["category"].iloc[k])) + "\n Title:" + str((df_update["title"].iloc[k])) + "\n Organiser:" + str((df_update["organiser"].iloc[k])) + "\n Date:" + str((df_update["date"].iloc[k])) + "\n Day:" + str((df_update["day"].iloc[k])) +  "\n Time:" + str((df_update["time"].iloc[k])) + "\n Address:" + str((df_update["address"].iloc[k])) + "\n Link:" + str((df_update["link"].iloc[k]))
-        send_telegram_message(msg, CHAT_ID, API_KEY)
+            msg = code_html + "\n\n Category: " + str((df_update["category"].iloc[k])) + "\n Title: " + str((df_update["title"].iloc[k])) + "\n Organiser: " + str((df_update["organiser"].iloc[k])) + "\n Date: " + str((df_update["date"].iloc[k])) + "\n Day: " + str((df_update["day"].iloc[k])) +  "\n Time: " + str((df_update["time"].iloc[k])) + "\n Address: " + str((df_update["address"].iloc[k])) + "\n Link: " + str((df_update["link"].iloc[k]))
+            send_telegram_message(msg, CHAT_ID, API_KEY, parse_mode= 'Markdown')
         
     # Save to csv
     df.to_csv("output.csv")
