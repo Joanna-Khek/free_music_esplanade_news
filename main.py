8e3bbef65e707fa6c5fe48e3ee85e5952d1f3706
Joanna <joannakhek@gmail.com>
2022-06-13 22:17:02 +0800

    formating

--- main.py
+++ main.py
@@ -131,8 +131,8 @@ if __name__ == "__main__":
     if len(update) != 0:
         
         df_update = df[df["title"].isin(update)]
-        code_html='*{}*'.format(df_update["title"].iloc[k])  
         for k in range(len(df_update)):
+            code_html='*{}*'.format(df_update["title"].iloc[k])  
             msg = code_html + "\n\n *Category:* " + str((df_update["category"].iloc[k])) + "\n *Title:* " + str((df_update["title"].iloc[k])) + "\n *Organiser:* " + str((df_update["organiser"].iloc[k])) + "\n *Date:* " + str((df_update["date"].iloc[k])) + "\n *Day:* " + str((df_update["day"].iloc[k])) +  "\n *Time:* " + str((df_update["time"].iloc[k])) + "\n *Address:* " + str((df_update["address"].iloc[k])) + "\n *Link:* " + str((df_update["link"].iloc[k]))
             send_telegram_message(msg, CHAT_ID, API_KEY)
         
