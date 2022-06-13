12b5ce19da1d9e23ec686b267866dc8bef4cc741
Joanna <joannakhek@gmail.com>
2022-06-13 16:35:21 +0800

    add argument

--- main.py
+++ main.py
@@ -75,6 +75,7 @@ if __name__ == "__main__":
     chrome_options.add_argument("--headless")
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument("disable-dev-shm-usage")
     #driver = webdriver.Chrome("chromedriver.exe", chrome_options=chrome_options)
     driver = webdriver.Chrome(executable_path=os.getenv("CHROMEDRIVER_PATH"), options=chrome_options)
     driver.get(url)
