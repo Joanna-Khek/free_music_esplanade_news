febcc4489e2d9f6c06fe7efe3f3384a7c00ec99a
Joanna <joannakhek@gmail.com>
2022-06-22 15:20:53 +0800

    update

--- main.py
+++ main.py
@@ -102,14 +102,14 @@ if __name__ == "__main__":
     # setting up options
     user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1216.0 Safari/537.2'
     chrome_options = webdriver.ChromeOptions()
-    #chrome_options.binary_location = os.getenv("GOOGLE_CHROME_BIN")
+    chrome_options.binary_location = os.getenv("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument(f'user-agent={user_agent}')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument("disable-dev-shm-usage")
-    driver = webdriver.Chrome("chromedriver.exe", chrome_options=chrome_options)
-    #driver = webdriver.Chrome(service=Service(os.getenv("CHROMEDRIVER_PATH")), options=chrome_options)
+    #driver = webdriver.Chrome("chromedriver.exe", chrome_options=chrome_options)
+    driver = webdriver.Chrome(service=Service(os.getenv("CHROMEDRIVER_PATH")), options=chrome_options)
     
     driver.get(url)
     WebDriverWait(driver, 100).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='card-root h-full w-full']")))
