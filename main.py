c3bf8fe33dbe77fb4b27abadd64ab710907dd658
Joanna Khek <joannakhek@gmail.com>
2022-11-30 14:22:35 +0800

    added chrome-binary to requirements

--- main.py
+++ main.py
@@ -156,12 +156,12 @@ if __name__ == "__main__":
         # Get only MUSIC category
         df_update = df_update[df_update["category"] == "Music"]
         print("Number of new music titles: {}".format(len(df_update)))
-        # for k in range(len(df_update)):
-        #     code_html='*{}*'.format(df_update["title"].iloc[k])  
-        #     msg = code_html + "\n\n *Category:* " + str((df_update["category"].iloc[k])) + "\n *Title:* " + str((df_update["title"].iloc[k])) + "\n *Organiser:* " + str((df_update["organiser"].iloc[k])) + "\n *Date:* " + str((df_update["date"].iloc[k])) + "\n *Address:* " + str((df_update["address"].iloc[k])) + "\n *Link:* " + str((df_update["link"].iloc[k]))
-        #     time.sleep(2)
-        #     send_telegram_message(msg, CHAT_ID, API_KEY)
-
+        for k in range(len(df_update)):
+            code_html='*{}*'.format(df_update["title"].iloc[k])  
+            msg = code_html + "\n\n *Category:* " + str((df_update["category"].iloc[k])) + "\n *Title:* " + str((df_update["title"].iloc[k])) + "\n *Organiser:* " + str((df_update["organiser"].iloc[k])) + "\n *Date:* " + str((df_update["date"].iloc[k])) + "\n *Address:* " + str((df_update["address"].iloc[k])) + "\n *Link:* " + str((df_update["link"].iloc[k]))
+            time.sleep(2)
+            send_telegram_message(msg, CHAT_ID, API_KEY)
+            print("Sent successfully!")
             
         # Save to csv
         print("Saving database...")
